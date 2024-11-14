0c0fbb911e7d0934863fba712ef0fa25fb87f48c
Gerard <gvanwengerden@live.nl>
2024-11-14 09:56:32 +0100

    Fix api test Clean up pom

--- src/main/java/steps/api/SearchSteps.java
+++ src/main/java/steps/api/SearchSteps.java
@@ -1,16 +1,17 @@
 package steps.api;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.microsoft.playwright.options.FormData;
 import com.microsoft.playwright.options.RequestOptions;
 import context.ScenarioContext;
 import context.SearchContext;
 import context.TestContext;
+import io.cucumber.core.internal.com.fasterxml.jackson.core.JsonProcessingException;
+import io.cucumber.core.internal.com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
 
-import java.util.HashMap;
+import java.util.List;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
@@ -25,13 +26,12 @@ public class SearchSteps {
 
     @When("^I search by api for (.*)$")
     public void iSearchForByApi(String searchTerm) {
-        var requestBody = new HashMap<>();
-        requestBody.put("search_product", searchTerm);
-
         var requestOptions = RequestOptions.create();
-        requestOptions.setHeader("content-type", "application/json");
-        //requestOptions.setQueryParam("search_product", searchTerm);
-        requestOptions.setData(requestBody);
+        var formData = FormData.create();
+        formData.set("search_product", searchTerm);
+
+        requestOptions.setHeader("content-type", "application/x-www-form-urlencoded");
+        requestOptions.setForm(formData);
         //TODO: move to separate api client
         var response = scenarioContext.requestContext.post("/api/searchProduct", requestOptions);
         assertThat(response).isOK();
@@ -40,9 +40,23 @@ public class SearchSteps {
     }
 
     @Then("^I should get an item with description (.*) and a price of (.*)$")
-    public void iShouldGetAnItemWithDescriptionDescriptionAndAPriceOfPrice(String expectedDescription, String expectedPrice) {
+    public void iShouldGetAnItemWithDescriptionDescriptionAndAPriceOfPrice(String expectedDescription, String expectedPrice) throws JsonProcessingException {
         var response = searchContext.apiResponse;
-        JsonObject json = new Gson().fromJson(response.text(), JsonObject.class);
-        Assert.assertTrue("expectedDescription:" + expectedDescription + ";actualDescription:" + response.text(), response.text().contains(expectedDescription));
+        var mapper = new ObjectMapper();;
+        var searchProducts = mapper.readValue(response.text(), SearchProducts.class);
+
+        Assert.assertTrue("expectedDescription:" + expectedDescription + ";actualDescription:" + response.text(),
+                searchProducts.products.stream().anyMatch(
+                        product -> product.name.equals(expectedDescription) && product.price.equals(expectedPrice)));
     }
+
+    //TODO: move to dto package
+    public record SearchProducts(int responseCode, List<Product> products){}
+
+    public record Product(int id, String name, String price, String brand, Category category){}
+
+    public record Category(UserType usertype, String category){}
+
+    public record UserType(String usertype){}
 }
+
